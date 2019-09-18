d9a1036fdc35ff09a6fbd9592b6c716c25b991cf
aj518480 <allisoncordovez@gmail.com>
2019-09-18 14:10:50 -0400

    added the progress of starting the JQuery to hook up on the pg

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-// Define global variables
-var queryUrl;
+//this is the information the user inputs
+$("#food-input")
+//this is ID of the button to sumbit the information the user inputs in
+$("#submit-button")
+//this is the ID for the AJAX call results (i.e:Restaurants Name,Address,Rating,IMG) to put on the pg
+$("#food-results")
