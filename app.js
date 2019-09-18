78f611c2b4199cf0642536891759afbd76f5f68e
aj518480 <allisoncordovez@gmail.com>
2019-09-18 14:14:51 -0400

    pseudocoded some future steps with the hook in with jquery

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 //this is the information the user inputs
+//this input will be connect to the results of thr ajax call
 $("#food-input")
+
 //this is ID of the button to sumbit the information the user inputs in
+//will be needing an .on (listen when the user submits the form)
 $("#submit-button")
+
 //this is the ID for the AJAX call results (i.e:Restaurants Name,Address,Rating,IMG) to put on the pg
+//will have to make some attributes for the images
 $("#food-results")
