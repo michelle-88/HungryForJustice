1c9b10d17e583751c72e60b37a042e0a5092502b
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-20 12:42:39 -0400

    created new array for latitude and longitude that stores the values as individual objects. added map method to go over locations array and run OPD Crimes API call at each index. Number of crimes are now appended to the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,12 @@
-// AJAX for OPD Crimes
-var latitude = "28.4329306890688";
-var longitude = "-81.4713156759186";
-
-var queryOPDUrl = "https://data.cityoforlando.net/resource/4y9m-jbmz.json?$where=within_circle(location," + latitude + "," + longitude + ",5000) and case_date_time > '2017-06' and case_offense_charge_type = 'Committed' and case_offense_category = 'Assault' and case_deposition = 'Arrest'";
-
-$.ajax({
-    url: queryOPDUrl,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-    // This will give us the number of crime reports in the specified area. Need to determine criteria of what's low/medium/high crime.
-    console.log(response.length); 
-});
+// Define global variables
+var latitude;
+var longitude;
+let locations = [];
+var searchTerm;
 
 // AJAX call for Yelp Fusion API
-var queryYelpUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=10&term=maggianos&location=orlando";
+var queryYelpUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=10&term=pizza&location=orlando";
+
 $.ajax({
     url: queryYelpUrl,
     headers: {
@@ -24,37 +16,122 @@ $.ajax({
 }).then(function(response){
     console.log(response);
 
-    for(var i = 0; i < response.businesses.length; i++){
+    // Loop through the Yelp API response to pull specific data points and dynamically add them to html page
+    for(var i = 0; i < response.businesses.length;i++){
+
+        // Create div for each restaurant from the response
         var foodDiv = $("<div>");
 
+        // Insert restaurant name, Yelp URL, Yelp image, address, and rating into dynamically created html elements
         var foodName = $("<h3>").text(response.businesses[i].name);
         var foodUrl = $("<a>").attr("href", response.businesses[i].url).append(foodName);
         var foodImg = $("<img class='food-image'>").attr("src", response.businesses[i].image_url);
         var foodAddress = $("<p>").text(response.businesses[i].location.display_address);
         var foodRating = $("<p>").text("Rating: " + response.businesses[i].rating + " out of 5");
 
+        // Assign a unique id to each result div so we can target it to append crime data later
+        foodDiv.attr("id", "result-num-" + i);
+
+        // Attach all restaurant info to it's individual div and then attach this div to the html page
         foodDiv.append(foodUrl, foodImg, foodAddress, foodRating);
         $("#food-results").append(foodDiv);
 
-        console.log(response.businesses[i].name);
-        console.log(response.businesses[i].url);
-        console.log(response.businesses[i].location.display_address);
-        console.log(response.businesses[i].rating);
-        console.log(response.businesses[i].image_url);
-        console.log(response.businesses[i].coordinates.latitude);
-        console.log(response.businesses[i].coordinates.longitude);
+        // Push the latitude and longitude for each restaurant into arrays to use later in the OPD Crimes API call
+        // latArray.push(response.businesses[i].coordinates.latitude);
+        // longArray.push(response.businesses[i].coordinates.longitude);
+        locations.push(
+            {
+                lat:response.businesses[i].coordinates.latitude,
+                long:response.businesses[i].coordinates.longitude
+            })
+         //locations array with locations[{lat:lat,long:long},{lat:lat,long:long},{lat:lat,long:long},{lat:lat,long:long}]
+        //getCrimesFunction that makes the call to the crimes api and builds the element
     }
+        console.log(locations)
+        locations.map((location, i) => {
+            //everything goes in here
+            //build the element
+            //make the call
+            //append to page
+            console.log(location.lat, location.long);
+        var queryOPDUrl = "https://data.cityoforlando.net/resource/4y9m-jbmz.json?$where=within_circle(location," + location.lat + "," + location.long + ",5000) and case_date_time > '2017-06' and case_offense_charge_type = 'Committed' and case_offense_category = 'Assault' and case_deposition = 'Arrest'";
+        
+        // AJAX call for OPD Crimes API
+        $.ajax({
+            url: queryOPDUrl,
+            method: "GET"
+        }).then(function(response){
+            console.log(response.length);
+
+            // Response.length is the number of crime reports in the specified area
+            // console.log(response.length);
+
+            var p = document.createElement("p");
+
+            // Append number of crimes to each restaurant's div
+            p.innerText = "Number of crimes in area: " + response.length; 
+            // $("#food-results").prepend(crimeNum);
+
+            console.log("p tag; ", p);
+          console.log(i);
+          
+            document.getElementById("result-num-" + i).appendChild(p); 
+            // $(document).find("#result-num-" + i).append(p);
+        });
+
+
+            
+        })
     
-});
+}) 
+
+
+// .then(function(){
+    
+   
+
+//     // Create for loop that will iterate 10 times
+//     for(var i = 0; i < 10; i++){
+//         //function
+//         uopdatePage(i);
+//         // Store latitude and longitude at each array index and store in variables to be used in OPD Crimes API call
+//         latitude = latArray[i];
+//         longitude = longArray[i];
+
+        
+ 
+
+//         var queryOPDUrl = "https://data.cityoforlando.net/resource/4y9m-jbmz.json?$where=within_circle(location," + latitude + "," + longitude + ",5000) and case_date_time > '2017-06' and case_offense_charge_type = 'Committed' and case_offense_category = 'Assault' and case_deposition = 'Arrest'";
+        
+//         // AJAX call for OPD Crimes API
+//         $.ajax({
+//             url: queryOPDUrl,
+//             method: "GET"
+//         }).then(function(response){
+//             // console.log(response);
+
+//             // Response.length is the number of crime reports in the specified area
+//             // console.log(response.length);
+
+           
+
+//             // Append number of crimes to each restaurant's div
+//             p.innerText = "Number of crimes in area: " + response.length; 
+//             // $("#food-results").prepend(crimeNum);
+
+//             console.log("p tag; ", p);
+          
+//             document.getElementById("result-num-" + i).appendChild(p); 
+            // $(document).find("#result-num-" + i).append(p);
+//         });
+//     }
+// })
 
 // Parameters needed for OPD Crimes API
     // API endpoint: https://data.cityoforlando.net/resource/4y9m-jbmz.json?
     // "$$app_token": rooCFXkjf9sgOxm15EGlRNlfE
-    // Use location (point data) OR location_zip parameter to find # of crimes in specified area?
     // when using location parameter, syntax is as follows: $where=within_circle(location,28.422239271,-81.332723878,1000)
         // This will pull crime cases with locations within the specified radius, which is in meters (we need to determine this radius)
-        // Coordinates are Latitude, Longitude
-    // Need to limit API results to crimes in last year
 
 // Parameters needed for Yelp API
     // API endpoint: https://api.yelp.com/v3/businesses/search
