0929e0426396903f972ce925b568532194dfe67e
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-21 12:54:56 -0400

    added modal to html page. added conditionals in submit button click listener to display modal when user enters invalid input in search field. otherwise, the application will run our API calls and display results.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,30 @@ var longitude;
 let locations = [];
 var searchTerm;
 
+// Click listener for food search submit button
 $("#submit-button").on("click", function(event){
+    // Prevent page from reloading when form is submitted
     event.preventDefault();
+
+    // Empty results div so only current 10 restaurant results will display
     $("#food-results").empty();
+
+    // Empty locations array so that the new 10 lat/long objects can be added
+    locations = [];
+    
+    // Grab user input from search field and store in variable to be inserted into AJAX queryUrl
     searchTerm = $("#food-input").val().trim();
-    console.log(searchTerm);
+
+    // Clear user input from search field after form is submitted
     $("#food-input").val("");
     
+    // If user does not enter anything into search field or if the their input includes numbers (i.e. is an address), then show modal and don't perform AJAX calls
+    if(searchTerm === "" || !isNaN(parseInt(searchTerm))){
+        $("#errorModal").modal('show');
+    }
+
+    else {
+
 // AJAX call for Yelp Fusion API
 var queryYelpUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=10&term=" + searchTerm + "&location=orlando";
 
@@ -28,7 +45,6 @@ $.ajax({
         // Create div for each restaurant from the response
         var foodDiv = $("<div class ='card rgba-blue-grey-light'>");
 
-
         // Insert restaurant name, Yelp URL, Yelp image, address, and rating into dynamically created html elements
         var foodName = $("<h3>").text(response.businesses[i].name);
         var foodUrl = $("<a target='blank'>").attr("href", response.businesses[i].url).append(foodName);
@@ -44,8 +60,6 @@ $.ajax({
         $("#food-results").append(foodDiv);
 
         // Push the latitude and longitude for each restaurant into arrays to use later in the OPD Crimes API call
-        // latArray.push(response.businesses[i].coordinates.latitude);
-        // longArray.push(response.businesses[i].coordinates.longitude);
         locations.push(
             {
                 lat:response.businesses[i].coordinates.latitude,
@@ -56,10 +70,6 @@ $.ajax({
     }
         console.log(locations)
         locations.map((location, i) => {
-            //everything goes in here
-            //build the element
-            //make the call
-            //append to page
             console.log(location.lat, location.long);
         var queryOPDUrl = "https://data.cityoforlando.net/resource/4y9m-jbmz.json?$where=within_circle(location," + location.lat + "," + location.long + ",5000) and case_date_time > '2017-06' and case_offense_charge_type = 'Committed' and case_offense_category = 'Assault' and case_deposition = 'Arrest'";
         
@@ -90,56 +100,6 @@ $.ajax({
     
 });
 
+}
+
 });
-
-// .then(function(){
-    
-   
-
-//     // Create for loop that will iterate 10 times
-//     for(var i = 0; i < 10; i++){
-//         //function
-//         uopdatePage(i);
-//         // Store latitude and longitude at each array index and store in variables to be used in OPD Crimes API call
-//         latitude = latArray[i];
-//         longitude = longArray[i];
-
-        
- 
-
-//         var queryOPDUrl = "https://data.cityoforlando.net/resource/4y9m-jbmz.json?$where=within_circle(location," + latitude + "," + longitude + ",5000) and case_date_time > '2017-06' and case_offense_charge_type = 'Committed' and case_offense_category = 'Assault' and case_deposition = 'Arrest'";
-        
-//         // AJAX call for OPD Crimes API
-//         $.ajax({
-//             url: queryOPDUrl,
-//             method: "GET"
-//         }).then(function(response){
-//             // console.log(response);
-
-//             // Response.length is the number of crime reports in the specified area
-//             // console.log(response.length);
-
-           
-
-//             // Append number of crimes to each restaurant's div
-//             p.innerText = "Number of crimes in area: " + response.length; 
-//             // $("#food-results").prepend(crimeNum);
-
-//             console.log("p tag; ", p);
-          
-//             document.getElementById("result-num-" + i).appendChild(p); 
-            // $(document).find("#result-num-" + i).append(p);
-//         });
-//     }
-// })
-
-// Parameters needed for OPD Crimes API
-    // API endpoint: https://data.cityoforlando.net/resource/4y9m-jbmz.json?
-    // "$$app_token": rooCFXkjf9sgOxm15EGlRNlfE
-    // when using location parameter, syntax is as follows: $where=within_circle(location,28.422239271,-81.332723878,1000)
-        // This will pull crime cases with locations within the specified radius, which is in meters (we need to determine this radius)
-
-// Parameters needed for Yelp API
-    // API endpoint: https://api.yelp.com/v3/businesses/search
-    // API key: nl17aOPr0p08nsjDyHPMrCNjgsc_8voZGJx1XttGzc0lhnb8EMhdl9UDgJH6szJH9wrv877dVxlBYbRjkr0AoZMu2_T9r0-0NJgkY2WwnSQMcZc0ZWw1YwJLwIqBXXYx
-    // Client ID: meg6MAQBLat4qpPNcZCiUw
